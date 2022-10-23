d5f9a4fdc383b68fcbfc26a79810147c080b6ca4
ChoiYoungHoon <dudgns2865@naver.com>
2022-10-23 17:31:48 +0900

    Initial commit

---
+++ src/main/java/doodoom/project/peermarket/PeerMarketApplication.java
@@ -0,0 +1,13 @@
+package doodoom.project.peermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeerMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/doodoom/project/peermarket/PeerMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package doodoom.project.peermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
