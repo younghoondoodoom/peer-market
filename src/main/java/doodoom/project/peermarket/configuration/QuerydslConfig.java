eb5b6aab24f3422e8d91e38a515f9c6860a3fbfb
ChoiYoungHoon <dudgns2865@naver.com>
2022-11-10 05:20:52 +0900

    chore: querydsl 설정
    querydsl을 사용하기 위한 설정
    - plugIn, dir 등 build.gradle 설정
    - config 파일 만들어 JPQQueryFactory 빈 등록
    - QFile 생성되는 디렉토리 .gitignore에 추가
    
    [skip ci]

---
+++ src/main/java/doodoom/project/peermarket/configuration/QuerydslConfig.java
@@ -0,0 +1,15 @@
+package doodoom.project.peermarket.configuration;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuerydslConfig {
+
+    @Bean
+    JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
+}
