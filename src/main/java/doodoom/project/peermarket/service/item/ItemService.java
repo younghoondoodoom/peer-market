27f99cc85fa8838daffac5d9689d93a00d980c64
ChoiYoungHoon <dudgns2865@naver.com>
2022-11-10 05:33:21 +0900

    feat(ItemService, ItemServiceImpl): item list를 반환하는 서비스 메소드 구현
    Item에서 ItemDto로 변환
    
    [skip ci]

--- src/main/java/doodoom/project/peermarket/service/item/ItemService.java
+++ src/main/java/doodoom/project/peermarket/service/item/ItemService.java
@@ -1,8 +1,13 @@
 package doodoom.project.peermarket.service.item;
 
+import doodoom.project.peermarket.dto.ItemDto;
 import doodoom.project.peermarket.dto.ItemRegisterInput;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ItemService {
 
     boolean register(ItemRegisterInput input, String email);
+
+    Page<ItemDto> list(Pageable pageable);
 }
--- src/main/java/doodoom/project/peermarket/service/item/ItemServiceImpl.java
+++ src/main/java/doodoom/project/peermarket/service/item/ItemServiceImpl.java
@@ -1,17 +1,20 @@
 package doodoom.project.peermarket.service.item;
 
+import static doodoom.project.peermarket.type.ItemStatus.ON_SALE;
+
 import doodoom.project.peermarket.core.file.FileLoader;
 import doodoom.project.peermarket.domain.Item;
 import doodoom.project.peermarket.domain.Member;
+import doodoom.project.peermarket.dto.ItemDto;
 import doodoom.project.peermarket.dto.ItemRegisterInput;
 import doodoom.project.peermarket.exception.member.MemberNotFoundException;
-import doodoom.project.peermarket.repository.ItemRepository;
-import doodoom.project.peermarket.repository.MemberRepository;
+import doodoom.project.peermarket.repository.item.ItemRepository;
+import doodoom.project.peermarket.repository.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import static doodoom.project.peermarket.type.ItemStatus.ON_SALE;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -25,17 +28,24 @@ public class ItemServiceImpl implements ItemService {
     @Override
     public boolean register(ItemRegisterInput input, String email) {
         Member member = memberRepository.findByEmail(email).orElseThrow(
-                MemberNotFoundException::new);
+            MemberNotFoundException::new);
         String imgName = fileLoader.upload(input.getFile());
         Item item = Item.builder()
-                .member(member)
-                .name(input.getName())
-                .description(input.getDescription())
-                .price(input.getPrice())
-                .imgName(imgName)
-                .status(ON_SALE)
-                .build();
+            .member(member)
+            .name(input.getName())
+            .description(input.getDescription())
+            .price(input.getPrice())
+            .imgName(imgName)
+            .status(ON_SALE)
+            .build();
         itemRepository.save(item);
         return true;
     }
+
+    @Override
+    public Page<ItemDto> list(Pageable pageable) {
+        Page<Item> items = itemRepository.findItemsOnSaleAndMemberActive(pageable);
+        return items.map(ItemDto::of);
+    }
+
 }
