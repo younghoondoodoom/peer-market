8337d006b0db54cee2847538b459c15232f62bfa
ChoiYoungHoon <dudgns2865@naver.com>
2022-11-10 20:53:36 +0900

    fix(ItemDto): itemdto 수정
    필요한 필드 넣어줌
    - createdAt, id
    
    [skip ci]

--- src/main/java/doodoom/project/peermarket/dto/ItemDto.java
+++ src/main/java/doodoom/project/peermarket/dto/ItemDto.java
@@ -1,6 +1,7 @@
 package doodoom.project.peermarket.dto;
 
 import doodoom.project.peermarket.domain.Item;
+import java.time.format.DateTimeFormatter;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,19 +15,23 @@ import lombok.Setter;
 @Builder
 public class ItemDto {
 
+    private Long id;
     private String name;
     private String description;
     private Long price;
     private String imgName;
+    private String  createdAt;
     private Long memberId;
     private String memberNickname;
 
     public static ItemDto of(Item item) {
         return ItemDto.builder()
+            .id(item.getId())
             .name(item.getName())
             .description(item.getDescription())
             .price(item.getPrice())
             .imgName(item.getImgName())
+            .createdAt(item.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")))
             .memberId(item.getMember().getId())
             .memberNickname(item.getMember().getNickname())
             .build();
